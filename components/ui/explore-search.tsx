1d299731d604b1b4d2320327ff5ad58f6abf6d6e
Christian Caneos <christiancaneos1@gmail.com>
2024-04-14 20:12:57 +0800

    feat: adjusted search ui

--- components/ui/explore-search.tsx
+++ components/ui/explore-search.tsx
@@ -38,12 +38,14 @@ const ExploreSearch = () => {
     },
   });
 
+  console.log(form.watch("search"));
+
   function onSubmit(data: z.infer<typeof FormSchema>) {}
 
   return (
     <Card className="my-4 w-full">
       <div className="flex items-center px-5 pb-2 pt-1">
-        <Popover>
+        <Popover open={open} onOpenChange={setOpen}>
           <PopoverTrigger className="group flex w-full items-center space-x-3">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -59,23 +61,30 @@ const ExploreSearch = () => {
                 d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
               />
             </svg>
-
             <Input
-              placeholder="Search..."
+              placeholder={open ? "" : "Search..."}
               readOnly
               className="rounded-none border-none bg-transparent pl-0 transition focus-visible:ring-0 focus-visible:ring-black"
             />
           </PopoverTrigger>
-          <PopoverContent sideOffset={12} className="w-[500px]">
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)}>
+          <PopoverContent
+            sideOffset={-40}
+            className="flex w-[548px] flex-col items-center space-y-3 border-none bg-transparent p-0 shadow-none"
+          >
+            <Form {...form}>G
+              <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
                 <FormField
                   control={form.control}
                   name="search"
                   render={({ field }) => (
-                    <FormItem>
+                    <FormItem className="px-[54px]">
                       <FormControl>
-                        <Input placeholder="search..." {...field} />
+                        <Input
+                          placeholder="Search..."
+                          autoComplete="off"
+                          className="rounded-none border-none bg-transparent pl-0 ring-offset-card transition focus-visible:ring-0 focus-visible:ring-transparent"
+                          {...field}
+                        />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
@@ -83,6 +92,7 @@ const ExploreSearch = () => {
                 />
               </form>
             </Form>
+            <div className="h-40 w-[85%] rounded-md border bg-popover"></div>
           </PopoverContent>
         </Popover>
       </div>
